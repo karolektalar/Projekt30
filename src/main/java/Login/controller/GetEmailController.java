53263b288e5f2b04ae29ae00ef41274be20801be
Karol Talarek <karol@puchaczy.waw.pl>
2017-01-04 15:21:28 +0100

    przypomnienie hasła

--- src/main/java/Login/controller/GetEmailController.java
+++ src/main/java/Login/controller/GetEmailController.java
@@ -1,14 +1,26 @@
 package Login.controller;
 
+import Login.domain.User;
+import Login.repository.UserRepository;
+import Login.service.mailUser.ResetPasswordService;
+import Login.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.Random;
 
-@SuppressWarnings("ALL")
-@Controller
+
+@RestController
 public class GetEmailController {
 //    private final UserService userService;
 //    private final UserCreateFormValidator userCreateFormValidator;
@@ -17,9 +29,43 @@ public class GetEmailController {
 //    public void initBinder(WebDataBinder binder) {
 //        binder.addValidators(userCreateFormValidator);
 //    }
+    private final UserService userService;
+    private JavaMailSender javaMailSender;
+    private UserRepository userRepository;
+
+//    @Autowired
+//    private ResetPasswordService resetPasswordService;
+
+    @Autowired
+    public GetEmailController(UserService userService, UserRepository userRepository,JavaMailSender javaMailSender) {
+        this.userService = userService;
+        this.userRepository = userRepository;
+        this.javaMailSender = javaMailSender;
+    }
+
+
+
 
     @RequestMapping("/getemail")
     public ModelAndView getGetemailPage(@RequestParam Optional<String> error) {
-        return new ModelAndView("getemail", "error", error);
+
+        SimpleMailMessage mail = new SimpleMailMessage();
+        Random gen = new Random();
+        CharSequence code;
+        code = Float.toString(gen.hashCode());
+        userService.getUserByEmail("dropboxjavaproject@gmail.com").
+                orElseThrow((() -> new NoSuchElementException(String.format("User=%s not found","dropboxjavaproject@gmail.com" ))))
+                .setPasswordHash(new BCryptPasswordEncoder().encode(code));
+
+        userRepository.save(userService.getUserByEmail("dropboxjavaproject@gmail.com").
+                orElseThrow((() -> new NoSuchElementException(String.format("User=%s not found","dropboxjavaproject@gmail.com" ))))
+        );
+
+        mail.setTo("dropboxjavaproject@gmail.com");
+        mail.setFrom("dropboxjavaproject@gmail.com");
+        mail.setSubject("Password reset");
+        mail.setText("Your new password is "+code);
+        javaMailSender.send(mail);
+        return new ModelAndView("redirect:/");
     }
 }
